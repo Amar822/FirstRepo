97aa26be27946db61aba526229c28a297834b80c
shiv <shiv@shiv-PC>
2018-11-12 09:40:43 +0530

    FirstRepo2

---
+++ src/HelloWorld.java
@@ -0,0 +1,36 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+
+
+
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+      
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+	 
+	        out.println("<h1>Hello World example using " +
+	   		"servlet interface.</h1>");
+	        out.close();	
+	}
+
+
+
+
+}
